007af6560e13f0b66fda9180462c7435ca5b9cfd
tkauer <tobias.kauer@fh-potsdam.de>
2019-07-03 14:34:53 +0200

    colors.js

---
+++ colors.js
@@ -0,0 +1,24 @@
+'use strict'
+
+var paxColors = {
+	"ceasefire": "#FFE41C",
+	"prenegotiation": "#FDB32F",
+	"framework-partial": "#ED7953",
+	"framework-comprehensive": "#CC4578",
+	"constitution": "#9C169E",
+	"implementation": "#5D00A6",
+	"renewal": "#0D0887",
+	"other": "#8C8C8C",
+
+	"humanRights": "#E4549B",
+	"military": "#A33A36",
+	"economic": "#BD6220",
+	"territorial": "#DFBA47",
+	"political": "#B5B867",
+	"institutions": "#469B7F",
+	"gender": "#335B8E",
+	"transitional": "#78377D",
+
+	"upwards": "#0075FF",
+	"downwards": "#FF3B00"
+}
